b585bd968bf5256667dda4c6933dab6a973d6ef3
donpsabance <donpsabance@gmail.co>
2020-09-09 21:17:08 -0700

    able to display data

--- scripts.js
+++ scripts.js
@@ -1,10 +1,55 @@
 $(document).ready(function () {
    google.maps.event.addDomListener(window, 'load', initializeAutoComplete);
+   document.getElementById('location_form_id').addEventListener('submit', function(e) {
+   		e.preventDefault();
+   		getWeatherData();
+   	}, false);
 });
 
 function initializeAutoComplete(){
 
-	var options = {types: ['(cities']};
+	// var options = {types: ['(cities']};
 	var input = document.getElementById('input');
 	var autoComplete = new google.maps.places.Autocomplete(input);
 }
+
+function getWeatherData(){
+	// document.getElementById('location_form_id').addEventListener('submit', function(e) {
+
+	// 	e.preventDefault();
+
+		var request = new XMLHttpRequest();
+		var url_1 = "http://api.openweathermap.org/data/2.5/weather?q=";
+		var url_3 = "&APPID=9a8d381120a94168102f65b0b3849164&units=metric";
+		var temp1 = url_1.concat(document.getElementById('input').value);
+		var request_url = temp1.concat(url_3);
+
+		request.onreadystatechange = function(){
+			 if(this.readyState == 4 && this.status == 200){
+
+				var data = JSON.parse(this.response);
+				var temp = data.main.temp;
+				var sunset = data.sys.sunset;
+
+				var date = new Date(sunset * 1000);
+				var hours = date.getHours();
+				var minutes = date.getMinutes();
+				var time = " " + hours + ":" + minutes;
+
+
+
+				document.getElementById("weather_temp").innerHTML = temp;
+				document.getElementById("weather_sunset").innerHTML = time;
+			}
+		};
+
+		request.open('GET', request_url, true);
+		request.send(null);
+	// }, false);
+}
+
+// function changeBackground(videoId, videoSrc){
+
+// 	var vid = document.getElementById(videoId).src = videoSrc;
+// 	vid.load();
+// }
